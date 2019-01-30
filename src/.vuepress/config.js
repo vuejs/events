93c26750d5d6afb877fa8391d8b12df15824524a
Ben Hong <bencodezen@gmail.com>
2019-01-29 23:45:22 -0500

    feature: Add Google Analytics

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    title: 'Vue.js Events',
-    themeConfig: {
-        logo: '/vue-events-logo.png'
-    }
+	title: 'Vue.js Events',
+	themeConfig: {
+		logo: '/vue-events-logo.png'
+	},
+	plugins: [
+		[
+			'@vuepress/google-analytics',
+			{
+				ga: 'UA-46852172-1'
+			}
+		]
+	]
 }
